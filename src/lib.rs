70f8c40cf725f0b97945f171dac3749ba6a30c85
Svyatoslav <slava-varov3@mail.ru>
2024-10-31 10:23:20 +0300

    `Vec::into_raw_parts` replaced with allocator api

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(vec_into_raw_parts)]
 #![allow(dead_code, clippy::missing_safety_doc)]
 
 mod simple;
@@ -28,56 +27,126 @@ impl TypeInfo {
 }
 
 pub struct Archetype {
-    pub(crate) component_types: Box<[TypeInfo]>,
     pub(crate) index: HashMap<TypeId, usize>,
+    pub(crate) component_types: Box<[TypeInfo]>,
     pub(crate) components: Box<[*mut u8]>,
-    pub(crate) len: usize,
     pub(crate) capacity: usize,
     pub(crate) entities: Vec<Entity>,
 }
 
 impl Archetype {
     pub fn new<C: Component>() -> Self {
-        let (ptr, len, cap) = Vec::<C>::new().into_raw_parts();
-
         Self {
             component_types: Box::new([TypeInfo::of::<C>()]),
             index: HashMap::from([(TypeId::of::<C>(), 0)]),
-            components: Box::new([ptr.cast()]),
-            len,
-            capacity: cap,
+            components: Box::new([std::ptr::null_mut()]),
+            capacity: 0,
             entities: vec![],
         }
+    }
+
+    pub fn alloc(&mut self, cap: usize) {
+        use std::alloc::{alloc, handle_alloc_error};
+
+        if cap == 0 {
+            return;
+        }
+
+        for (type_info, components_ptr) in
+            self.component_types.iter().zip(self.components.iter_mut())
+        {
+            let Ok(layout) =
+                Layout::from_size_align(cap * type_info.layout.size(), type_info.layout.align())
+            else {
+                continue;
+            };
+
+            let ptr = unsafe { alloc(layout) };
+
+            if ptr.is_null() {
+                handle_alloc_error(layout);
+            }
+
+            *components_ptr = ptr;
+        }
+
+        self.capacity = cap;
+    }
+
+    pub fn realloc(&mut self) {
+        use std::alloc::{handle_alloc_error, realloc};
+
+        let next_capacity = 3 * (self.capacity + 1) / 2;
+
+        for (type_info, components_ptr) in
+            self.component_types.iter().zip(self.components.iter_mut())
+        {
+            let Ok(prev_layout) = Layout::from_size_align(
+                self.capacity * type_info.layout.size(),
+                type_info.layout.align(),
+            ) else {
+                continue;
+            };
+
+            let ptr = unsafe {
+                realloc(
+                    *components_ptr,
+                    prev_layout,
+                    next_capacity * type_info.layout.size(),
+                )
+            };
+
+            if ptr.is_null() {
+                handle_alloc_error(prev_layout);
+            }
+
+            *components_ptr = ptr;
+        }
+
+        self.capacity = next_capacity;
     }
 
     pub fn add_component<C: Component>(&mut self, value: C) {
         assert_eq!(self.component_types.len(), 1);
 
+        if self.capacity == 0 {
+            self.alloc(1);
+        } else if self.entities.len() == self.capacity {
+            self.realloc();
+        }
+
         let index = self.index[&TypeId::of::<C>()];
+        let ptr = self.components[index].cast::<C>();
 
-        let mut vec = unsafe {
-            Vec::<C>::from_raw_parts(self.components[index].cast(), self.len, self.capacity)
-        };
-        vec.push(value);
-
-        let (ptr, len, capacity) = vec.into_raw_parts();
-
-        self.components[index] = ptr.cast();
-        self.len = len;
-        self.capacity = capacity;
+        unsafe {
+            ptr.add(self.entities.len()).write(value);
+        }
     }
 }
 
 impl Drop for Archetype {
     fn drop(&mut self) {
+        use std::alloc::dealloc;
+
         for (i, &components_ptr) in self.components.iter().enumerate() {
-            let size = self.component_types[i].layout.size();
+            let elem_layout = self.component_types[i].layout;
+            let size = elem_layout.size();
             let drop = self.component_types[i].drop;
 
-            for j in 0..self.len {
+            let Ok(layout) =
+                Layout::from_size_align(self.capacity * elem_layout.size(), elem_layout.align())
+            else {
+                continue;
+            };
+
+            for j in 0..self.entities.len() {
                 unsafe {
                     drop(components_ptr.add(size * j));
                 }
+            }
+
+            unsafe {
+                dealloc(components_ptr, layout);
             }
         }
     }
@@ -114,7 +183,7 @@ impl World {
             }
         };
 
-        let entity_index = self.archetypes[archetype_index].len;
+        let entity_index = self.archetypes[archetype_index].entities.len();
         let location = Location {
             entity_index: entity_index as u32,
             archetype_index: archetype_index as u32,
@@ -132,7 +201,7 @@ impl World {
         let archetype_index = self.index[&[TypeId::of::<C>()][..]];
         let components_index = self.archetypes[archetype_index].index[&TypeId::of::<C>()];
         let components_ptr = self.archetypes[archetype_index].components[components_index];
-        let components_len = self.archetypes[archetype_index].len;
+        let components_len = self.archetypes[archetype_index].entities.len();
         let components =
             unsafe { slice::from_raw_parts_mut(components_ptr.cast(), components_len) };
 
@@ -158,6 +227,10 @@ mod tests {
         struct Speed(f32);
         impl Component for Speed {}
 
+        #[derive(Debug, PartialEq)]
+        struct Tag;
+        impl Component for Tag {}
+
         let mut world = World::default();
 
         let entities = [
@@ -166,6 +239,7 @@ mod tests {
             world.spawn(Speed(42.0)),
             world.spawn(Name(String::from("Third"))),
             world.spawn(Speed(69.0)),
+            world.spawn(Tag),
         ];
 
         assert_eq!(
@@ -183,6 +257,11 @@ mod tests {
                 (entities[2], &mut Speed(42.0)),
                 (entities[4], &mut Speed(69.0)),
             ],
-        )
+        );
+
+        assert_eq!(
+            world.query_mut::<Tag>().collect::<Vec<_>>(),
+            [(entities[5], &mut Tag)],
+        );
     }
 }
